4c5066cf926410f83ffa6b34514576f96151adf4
georgezampakikas <gzampakikas@medicalinfo.eu>
2025-09-01 16:02:42 +0300

    feat: auto height table

--- src/app/components/general-instructions/general-instructions.ts
+++ src/app/components/general-instructions/general-instructions.ts
@@ -1,38 +1,40 @@
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NsAutoHeightTableDirective } from "../../directives/ns-auto-height-table";
 
 @Component({
   selector: 'app-general-instructions',
   imports: [
-    NzTableModule
-  ],
+    NzTableModule,
+    NsAutoHeightTableDirective
+],
   templateUrl: './general-instructions.html',
   styleUrl: './general-instructions.scss'
 })
 export class GeneralInstructions {
-  public tableHeight!: number;
-  @ViewChild('tableWrapper') tableWrapper!: ElementRef<HTMLDivElement>;
+  // public tableHeight!: number;
+  // @ViewChild('tableWrapper') tableWrapper!: ElementRef<HTMLDivElement>;
 
-  ngAfterViewInit() {
-    this.calculateTableHeight();
-  }
+  // ngAfterViewInit() {
+  //   this.calculateTableHeight();
+  // }
 
-  @HostListener('window:resize')
-  onResize() {
-    this.calculateTableHeight();
-  }
+  // @HostListener('window:resize')
+  // onResize() {
+  //   this.calculateTableHeight();
+  // }
 
-  calculateTableHeight() {
-    if (!this.tableWrapper) return;
+  // calculateTableHeight() {
+  //   if (!this.tableWrapper) return;
 
-    const rect = this.tableWrapper.nativeElement.getBoundingClientRect();
-    const offsetTop = rect.top;
+  //   const rect = this.tableWrapper.nativeElement.getBoundingClientRect();
+  //   const offsetTop = rect.top;
 
-    const viewportHeight = window.innerHeight;
+  //   const viewportHeight = window.innerHeight;
 
-    this.tableHeight = viewportHeight - offsetTop - 64; // minus header space;
-    console.log('Table height:', this.tableHeight);
-  }
+  //   this.tableHeight = viewportHeight - offsetTop - 64; // minus header space
+  //   console.log('Table height:', this.tableHeight);
+  // }
 
   listOfData = [
     {
